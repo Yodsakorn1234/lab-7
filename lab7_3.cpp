38f3362f4b74353b240ab5cf6e538b7058568eef
Yodsakorn <menming1234@outlook.com>
2021-11-30 17:59:36 +0700

    lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,42 @@
 
 using namespace std;
 
+int adiff(int a, int b)
+  {
+  a = a%360;
+  b = b%360;
+  //cout << a << " " << b << endl;
+  if (a < 0){
+    a = 360 + a;
+  }
+  if (b < 0)
+  {
+    b = 360 + b;
+  }
+  //cout << a << " " << b << endl;
 
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  int min,max;
+  if (a < b)
+  {
+    min = a;
+    max = b;
+  }
+  else{
+    min = b;
+    max = a;
+  }
+  //cout << min << " " << max << endl;
+  
+  if (min+180 == max)
+  {
+    return 180;
+  }
+  else if (min+180 < max)
+  {
+    return abs(360+min-max);
+  }
+  else
+  {
+    return abs(max-min);
+  }
 }
